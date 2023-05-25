b02f3d49dcaf54ff66ec43f923fe7277b03a82d2
BruceNTB <nfttop.best@gmail.com>
2023-05-25 23:24:29 +0800

    ✨ add content page

--- projects/01-NFT-Fi-Twitter/code/src/contentScripts/App.vue
+++ projects/01-NFT-Fi-Twitter/code/src/contentScripts/App.vue
@@ -1,29 +1,26 @@
 <script setup lang="ts">
-import { useToggle } from '@vueuse/core'
-import 'uno.css'
+import '../styles'
 
-const [show, toggle] = useToggle(false)
+const show = ref(false)
 </script>
 
 <template>
-  <div class="flex font-sans m-5 right-0 bottom-0 leading-1em z-100 fixed items-end select-none">
-    <Web3Inject />
+  <div class="flex font-sans top-0 right-0 leading-1em z-100 fixed items-end select-none">
     <div
+      v-show="show"
       class="bg-white rounded-lg h-min shadow w-max text-gray-800"
       p="x-4 y-2"
       m="y-auto r-2"
       transition="opacity duration-300"
-      :class="show ? 'opacity-100' : 'opacity-0'"
     >
-      <h1 class="text-lg">
-        Vitesse WebExt
-      </h1>
-      <SharedSubtitle />
+      <ContentList />
     </div>
+
     <button
+      v-show="!show"
       class="border-none rounded-full cursor-pointer flex h-10 shadow w-10"
       bg="teal-600 hover:teal-700"
-      @click="toggle()"
+      @click="show = true"
     >
       <pixelarticons-power class="m-auto text-white text-lg block" />
     </button>

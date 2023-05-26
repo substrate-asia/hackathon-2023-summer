475d0f526c0d477239925f58d58687ffc8a93fd0
BruceNTB <nfttop.best@gmail.com>
2023-05-27 03:40:17 +0800

    ✨ add tab for list

---
+++ projects/01-NFT-Fi-Twitter/code/src/components/content/Main.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const tabs = [
+  { name: 'Applied', href: '#', count: '52', current: false },
+  { name: 'Phone Screening', href: '#', count: '6', current: false },
+  { name: 'Interview', href: '#', count: '4', current: true },
+  { name: 'Offer', href: '#', current: false },
+  { name: 'Disqualified', href: '#', current: false },
+]
+</script>
+
+<template>
+  <div>
+    <div class="sm:hidden">
+      <label for="tabs" class="sr-only">Select a tab</label>
+      <select id="tabs" name="tabs" class="rounded-md border-gray-300 text-base w-full py-2 pr-10 pl-3 block sm:text-sm focus:outline-none focus:border-indigo-500 focus:ring-indigo-500">
+        <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">
+          {{ tab.name }}
+        </option>
+      </select>
+    </div>
+    <div class="hidden sm:block">
+      <div class="border-b border-gray-200">
+        <nav class="flex -mb-px space-x-8" aria-label="Tabs">
+          <a v-for="tab in tabs" :key="tab.name" href="#" class="flex font-medium border-b-2 text-sm py-4 px-1 whitespace-nowrap" :class="[tab.current ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700']" :aria-current="tab.current ? 'page' : undefined">
+            {{ tab.name }}
+            <span v-if="tab.count" class="rounded-full font-medium text-xs ml-3 py-0.5 px-2.5 hidden md:inline-block" :class="[tab.current ? 'bg-indigo-100 text-indigo-600' : 'bg-gray-100 text-gray-900']">{{ tab.count }}</span>
+          </a>
+        </nav>
+      </div>
+      <ContentList />
+    </div>
+  </div>
+</template>
--- projects/01-NFT-Fi-Twitter/code/src/contentScripts/App.vue
+++ projects/01-NFT-Fi-Twitter/code/src/contentScripts/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import '../styles'
 
-const showList = ref(false)
+const showContent = ref(false)
 const showWrite = ref(true)
 const doClose = () => {
-  showList.value = false
+  showContent.value = false
   showWrite.value = false
 }
 </script>
@@ -12,7 +12,7 @@ const doClose = () => {
 <template>
   <div class="flex font-sans top-0 right-0 leading-1em z-999 fixed items-end select-none">
     <div
-      v-show="showList || showWrite"
+      v-show="showContent || showWrite"
       class="bg-white rounded-lg h-min shadow w-max text-gray-800 relative"
       p="x-4 y-2"
       m="y-auto r-2"
@@ -21,15 +21,15 @@ const doClose = () => {
       <button type="button" class="rounded-full bg-indigo-600 shadow-sm text-white p-2 -left-10 absolute hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 " @click="doClose">
         <ic-round-close class="h-5 w-5" aria-hidden="true" />
       </button>
-      <ContentList v-show="showList" />
+      <ContentMain v-show="showContent" />
       <FormMain v-show="showWrite" />
     </div>
 
-    <div v-show="!showList && !showWrite">
+    <div v-show="!showContent && !showWrite">
       <button
         class="border-none rounded-full cursor-pointer flex h-10 shadow mb-2 w-10"
         bg="indigo-600 hover:indigo-700"
-        @click="showList = true"
+        @click="showContent = true"
       >
         <ic-outline-format-list-bulleted class="m-auto text-white text-lg block" />
       </button>

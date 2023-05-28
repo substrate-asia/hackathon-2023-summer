aeea29614d647d96a61448b80723f6dde677aff8
BruceNTB <nfttop.best@gmail.com>
2023-05-29 01:05:26 +0800

    ✨ finish onboard ux

--- projects/01-NFT-Fi-Twitter/code/src/components/onboarding/header.vue
+++ projects/01-NFT-Fi-Twitter/code/src/components/onboarding/header.vue
@@ -3,12 +3,12 @@ interface Props {
   title?: string
 }
 const {
-  title = "On board to Web3",
+  title = 'On board to Web3',
 } = defineProps<Props>()
 </script>
 
 <template>
-  <div class="mt-1/10 sm:mx-auto sm:max-w-sm sm:w-full">
+  <div class="sm:mx-auto sm:max-w-lg sm:w-full">
     <img class="rounded-full mx-auto h-20 w-auto" src="https://avatars.githubusercontent.com/u/109504677?v=4" alt="Web3Hacker.World">
     <h2 class="font-bold mt-10 text-center text-white tracking-tight text-2xl leading-9">
       {{ title }}

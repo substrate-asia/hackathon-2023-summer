11862416387ba2a1390600d05db28ba7b7728201
JIM__HUANG <jimhuang@cherrypicks.com>
2023-06-23 19:34:16 +0800

    chore: update i18n

--- projects/24-evmscan/apps/evmscan_frontend/next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-  i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'zh-CN'],
-  },
-  localePath: path.resolve(__dirname, 'public', 'locales'),
-};
--- projects/24-evmscan/apps/evmscan_frontend/src/app/[lang]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getDictionary } from '../../../get-dictionary'
-import { Locale } from '../../../i18n-config'
-
-export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
-  const dictionary = await getDictionary(lang)
-  return <div>{dictionary.home}</div>
-}

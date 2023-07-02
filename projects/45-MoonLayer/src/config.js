bd2dce470cbdf0b6c0380a0d6116b725e12cde37
Gin F <109033319+GinFdev@users.noreply.github.com>
2023-07-02 03:29:49 -0400

    Node configuration

---
+++ projects/45-MoonLayer/src/config.js
@@ -0,0 +1,37 @@
+export default {
+    // Ethereum account info
+    ETH_ADDRESS: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    ETH_PRIVKEY: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+    ETH_RPC_URL: "http://127.0.0.1:8545/",
+    CHAIN_ID: 31337,
+
+    // Moon Layer account info
+    PRIVKEY: "0x3f1c78a7c6c46e3d8a45b1bc29048719bff0903375cd066ccb7675ce4c77752e",
+    
+    // Sequencer config
+    SEQUENCER_MODE: true,
+    // Address to receive tx fees
+    SEQUENCER_ADDRESS: "0xb8938A1082389f5881dF89980210A8901328B291",
+
+    // Rollup config
+    ROLLUP_ADDRESS: "0x029B93211e7793759534452BDB1A74b58De22C9c",
+    
+    // Sync config
+    START_SYNC: 1,
+
+    // RPC server config
+    RPC_PORT: 3000,
+
+    // DB Path
+    LEVEL_PATH: "C:/Users/npqua/desktop/MoonLayer/leveldb",
+    LOG_FILE: "C:/Users/npqua/desktop/MoonLayer/log",
+    ADDRESS_DB_PATH: "C:/Users/npqua/desktop/MoonLayer/addressdb",
+    INDEX_DB_PATH: "C:/Users/npqua/desktop/MoonLayer/indexdb",
+
+    // Genesis details
+    FIRST_MINT_ADDR: "0xa39372a91baa8dcad7ee9da3d8311cf249dded06",
+    FIRST_MINT_AMOUNT: "100000000000000000000000000",
+
+    // RPC config
+    EXPORT_PRIVKEY: false
+}

0948657fecd54cc1ca37a638b7c48183825ce324
xiaofeng pan <wbug@163.com>
2023-07-04 00:01:40 +0800

    index.js

--- projects/CryptoSpiral/src/spiral-front/index.js
+++ projects/CryptoSpiral/src/spiral-front/index.js
@@ -27,6 +27,79 @@ class PolkadotWeb3JSSample {
 
 //some code have been removed here for it is just under test
 //but it exist in the released binary (dist/index_substrate.js)
+//some key code is below:
+
+	async getPhalaDaoContractInMap(contractId) {
+		if(!window.polkadotPhalaDaoContractMap){
+
+			window.polkadotPhalaDaoContractMap = new Map();
+		} 
+		
+		if(!window.polkadotPhalaDaoContractMap.get(contractId)){
+			const api = await this.getPhalaApiInMap(contractId);	
+			const pruntimeURL = 'https://poc5.phala.network/tee-api-1';  // assuming the default port
+			
+			
+			const contract = new ContractPromise(
+				//await create({api, baseURL: pruntimeURL, contractId}),
+				(await create({ api, baseURL: pruntimeURL, contractId })).api,
+				this.metadata,
+				contractId
+			);
+
+
+			window.polkadotPhalaDaoContractMap.set(contractId, contract);
+			
+
+			
+		}
+		//console.log("a333");
+		return window.polkadotPhalaDaoContractMap.get(contractId);
+
+	}
+
+
+
+	async check_certificate_phala(api, PHRASE){
+		var certificate = null;
+
+		// finds an injector for an address
+		const address = PHRASE;
+		await web3Enable('web3_promotion');
+		const injector = await web3FromAddress(address);//user
+		var account= {};
+		account.address = address;
+		// sets the signer for the address on the @polkadot/api
+		certificate = await signCertificate({	api,  signer: injector.signer, account});
+		
+
+		return certificate;
+
+	}
+	
+
+	// This is Verification & Reward function , it will call Phala Contract , 
+	// On-chain operation definition: Allows advertisers to quickly define on-chain operations that users need to complete.
+	// Verification & Reward: When calling this API, it indicates whether the specified user has completed the operation defined in step 1. If successful, the predefined 
+	async doTaskActionInTEE(PHRASE, contractId, taskId, actiionId,  tokenmd5, inviteAccountId) {
+		console.log(`doTaskActionInTEE taskId ${taskId}, actiionId ${actiionId},  tokenmd5  ${tokenmd5}, inviteAccountId  ${inviteAccountId} `);
+		
+
+		const api = await this.getPhalaApiInMap(contractId);	
+		//console.log(api);
+		const contract = await this.getPhalaDaoContractInMap(contractId);
+
+		const certificate = await this.check_certificate_phala(api, PHRASE);
+
+		const outcome = await contract.query.doTaskAction(certificate, {}, taskId, actiionId, tokenmd5, inviteAccountId);
+
+		
+		
+		return outcome;	
+
+	}
+	
+	
 
 
 

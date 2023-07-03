53ee65cc5b502c85e2387b4d5be09d21ab1ad201
John^2 Li <hokage@deva.ltd>
2023-07-03 23:21:18 +0800

    ai-readme

---
+++ projects/37-Pivot/src/ai/pivot_ai.py
@@ -0,0 +1,48 @@
+
+import typer
+from loguru import logger
+app = typer.Typer()
+import pandas as pd
+
+
+
+@app.command()
+def check_is_equal(first_file=None, second_file=None):
+    df1 = pd.read_excel(first_file)
+    all_counts = df1.shape[0]
+    df2 = pd.read_excel(second_file)
+    df_all = pd.merge(df1, df2, on= "id")
+    falcon_counts = df_all[df_all["falcon_answer_x"] == df_all["falcon_answer_y"]].shape[0]
+    if falcon_counts == all_counts:
+        logger.info(f"{first_file} falcon_answer equal {second_file} ; total_counts: {all_counts}, falcon_counts: {falcon_counts} ")
+    else:
+        logger.warning(f"{first_file} falcon_answer not equal {second_file} please check it ")
+
+@app.command()
+def check_is_equal(first_file=None, second_file=None):
+    df1 = pd.read_excel(first_file)
+    all_counts = df1.shape[0]
+    df2 = pd.read_excel(second_file)
+    df_all = pd.merge(df1, df2, on= "id")
+    falcon_counts = df_all[df_all["falcon_answer_x"] == df_all["falcon_answer_y"]].shape[0]
+    if falcon_counts == all_counts:
+        logger.info(f"{first_file} falcon_answer equal {second_file} ; total_counts: {all_counts}, falcon_counts: {falcon_counts} ")
+    else:
+        logger.warning(f"{first_file} falcon_answer not equal {second_file} please check it ")
+
+def comression_rate(file=None):
+    df = pd.read_excel(file)
+    total_rows = df.shape[0]
+    correct_rows = df[df["falcon_answer"] == df["correct_answer"]].shape[0]
+    model_parameters = df["model_parameters"].iloc[0]
+    training_set = df["training_set"].iloc[0]
+    correct_rate = correct_rows/total_rows
+    comression_rate = 1 - model_parameters / training_set * correct_rate
+    comression_rate = int(comression_rate * 100)
+    logger.info(f"comression_rate is {comression_rate} %")
+
+if __name__ == '__main__':
+    app()
+
+
+
